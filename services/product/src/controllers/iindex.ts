82c14988f3237125c65c3a16efceecf64520fafd
Safayet <hossainsafayet187@gmail.com>
2025-10-04 00:41:11 +0600

    module export in controller

--- services/product/src/controllers/iindex.ts
+++ services/product/src/controllers/iindex.ts
@@ -1,1 +1,3 @@
 export { default as createProduct } from "./createProduct";
+export { default as getProductDetails } from "./getProductDetails";
+export { default as getProducts } from "./getProducts";
